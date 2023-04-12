e55249acd013c5a872dd053282917451cae28b39
monk3yd <jegajardog@gmail.com>
2023-04-12 10:09:31 -0300

    Add and tweak setup scripts

--- scripts/update-lambda.sh
+++ scripts/update-lambda.sh
@@ -2,6 +2,6 @@
 
 LAMBDA_NAME="fastapi-lambda"
 BUCKET_NAME="fastapi-lambda-sandbox"
-KEY="code/lambda.zip"
+S3_KEY="code/lambda.zip"
 
-aws lambda update-function-code --function-name ${LAMBDA_NAME} --s3-bucket ${BUCKET_NAME} --s3-key ${KEY}
+aws lambda update-function-code --function-name ${LAMBDA_NAME} --s3-bucket ${BUCKET_NAME} --s3-key ${S3_KEY}
